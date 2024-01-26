8c91c404c58454d31562f6442f6dd9e3c4426589
ExiqonBot <155193519+ExiqonBot@users.noreply.github.com>
2024-01-26 02:43:56 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -16,7 +16,7 @@ global.packname = 'Sticker By'
 global.author = 'ღƐ×ͥΐզͣօͫή BØ₸ღ'
 //console view/theme
 global.themeemoji = '👑'
-global.wm = "Exiqon botz inc."
+global.wm = "ღĹíͥőͣńͫღ botz inc."
 
 //theme link
 global.link = 'https://chat.whatsapp.com/EGqCW9HeVoq3W2KmgX3Ri0'
@@ -38,7 +38,7 @@ global.autoread_status = false //auto view status/story
 
 //reply messages
 global.mess = {
-    done: '*here you go!* \n\n*🎀 ღƐ×ͥΐզͣօͫή BØ₸ღ 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
+    done: '*here you go!* \n\n*🎀 ღĹíőͥńͣ BͫØ₸ღ 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/ExiqonBot/Bot-Launch-V1\n',
     prem: '*Dieser Befehl kann nur von Premium-Nutzern verwendet werden.*',
     admin: '*Dieser Befehl kann nur von einem Admin verwendet werden.*',
     botAdmin: '*Dieser Befehl kann nur verwendet werden wenn der Bot Gruppenadmin ist.* ',
